acce6b143ce9ffa080af939f41d376e002355875
Adriana Vento Conesa <adrvencon@alum.us.es>
2024-05-31 18:28:08 +0200

    feat: :sparkles: add arc fit and support for informal tests

--- utils/clusters_print.py
+++ utils/clusters_print.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-def print_clusters(result_clusters, assigned_points, centers, radii):
+def print_clusters(result_clusters, assigned_points, centers=[], radii=[]):
     for i, cluster in enumerate(result_clusters):
-        center_error = np.linalg.norm(centers[i] - cluster.center)
-        radius_error = np.abs(radii[i] - cluster.radius)
-        print(f"CLUSTER {i+1}:")
-        print(f"Estimated Center = {cluster.center}, Estimated Radius = {cluster.radius}") 
-        print("------------------------------------------------------------------")
-        print(f"Center Error = {center_error}, Radius Error = {radius_error}")
-        print("------------------------------------------------------------------")
-        print(f"Assigned points = {assigned_points}")
-        print("")
+
+        if len(centers) != 0:
+            center_error = np.linalg.norm(centers[i] - cluster.center)
+            radius_error = np.abs(radii[i] - cluster.radius)
+            accumulated_error = center_error + radius_error
+            print(f"CLUSTER {i+1}:")
+            print(f"Estimated Center = {cluster.center}, Estimated Radius = {cluster.radius}") 
+            print("------------------------------------------------------------------")
+            print(f"Center Error = {center_error}, Radius Error = {radius_error}, Accumulated Error = {accumulated_error}")
+            print("------------------------------------------------------------------")
+            print(f"Assigned points = {assigned_points}")
+            print("")
+        else:
+            print(f"CLUSTER {i+1}:")
+            print(f"Estimated Center = {cluster.center}, Estimated Radius = {cluster.radius}") 
+            print("------------------------------------------------------------------")
+            print(f"Assigned points = {assigned_points}")
+            print("")
+
+
